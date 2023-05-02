1951ec7eb1efa201e890c8d065b0f33618a6371a
samaelgomez <samaelgomezherrera.com>
2023-05-02 11:25:40 +0200

    Mantine UI integrated in the application

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { MantineProvider } from '@mantine/core';
 import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-    <App />
+  <MantineProvider withGlobalStyles withNormalizeCSS>
+    <MantineProvider>
+      <App />
+    </MantineProvider>
+  </MantineProvider>
   // </React.StrictMode>,
 )
